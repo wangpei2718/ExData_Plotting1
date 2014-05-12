a2c6edb593447af0659498a5460902574a902e46
wangpei2718 <wangpei2718@qq.com>
2014-05-12 08:49:59 +0800

    plot 1, this time in R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\ฑฆมี\\Documents\\ณ๖น๚\\coursera\\ex\\exdata-data-household_power_consumption")
+x<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+x$Date<-as.Date(as.character(x$Date),format="%d/%m/%Y")
+y<-subset(x,x$Date=="2007-02-01")
+z<-subset(x,x$Date=="2007-02-02")
+m<-rbind(y,z)
+m$date_time<-as.POSIXct(paste(m$Date,as.character(m$Time)))
+m$Global_active_power<-as.numeric(levels(m$Global_active_power))[m$Global_active_power]
+m$Global_reactive_power<-as.numeric(levels(m$Global_reactive_power))[m$Global_reactive_power]
+
+m$Voltage<-as.numeric(levels(m$Voltage))[m$Voltage]
+m$Sub_metering_1<-as.numeric(levels(m$Sub_metering_1))[m$Sub_metering_1]
+m$Sub_metering_2<-as.numeric(levels(m$Sub_metering_2))[m$Sub_metering_2]
+
+png(filename="plot1.png",width=480,height=480,units="px")
+windows()
+hist(m$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
+dev.off()
